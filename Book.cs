d55e9f8ef21a1e2a80e63201a8934c072e00668d
Spitzy <edipeev123@gmail.com>
2026-01-06 19:24:18 +0200

    Getters And Setters

--- Book.cs
+++ Book.cs
@@ -4,23 +4,12 @@ namespace Student_Library_Assistant;
 
 public class Book
 {
-    private string title;
-    private int year;
-    private string author;
-
-    public int BookId { get; set; }
+    public int Id { get; set; }
     public string Title { get; set; }
     public int Year { get; set; }
     public string Author { get; set; }
     
-    public Book(int id, string bookTitle, int bookYear, string bookAuthor)
-    {
-        BookId = id;
-        Title = bookTitle;
-        Year = bookYear;
-        Author = bookAuthor;
-    }
-    
+    public bool IsAvailable { get; set; }
 }
 
 // When deleting a book:
--- Student.cs
+++ Student.cs
@@ -2,26 +2,11 @@ namespace Student_Library_Assistant;
 
 public class Student
 {
-    public int BookId
-    { get; set; }
-
-    public string Name
-    { get; set; }
-
-    public int Year
-    { get; set; }
-    
-    public string FacultyNumber
-    { get; set; }
-    
-    
-    public Student(int id, string studentName, int studentYear, string facultyNumber)
-    {
-        BookId = id;
-        Name = studentName;
-        Year = studentYear;
-        FacultyNumber = facultyNumber;
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public string FacultyNumber { get; set; }
+    public int BorrowedBooks {get; set; }
 }
 
 // When deleting a student:
